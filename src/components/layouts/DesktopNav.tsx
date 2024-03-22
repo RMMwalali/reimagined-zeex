dcfdccaf9c54439282123251bef677b7b113fb3e
rmmwalali <2003285@students.kcau.ac.ke>
2024-03-22 13:50:02 +0300

    j

--- src/components/layouts/DesktopNav.tsx
+++ src/components/layouts/DesktopNav.tsx
@@ -29,7 +29,7 @@ const DesktopNav = () => {
                     <Link className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md' href='/'>
                       <Icons.logo className='h-6 w-6' />
                       <div className='mb-2 mt-4 text-lg font-medium'>Circuit City</div>
-                      <p className='text-sm leading-tight text-muted-foreground'>Welcome to our laptop emporium, where we have revolutionized the way you shop for your perfect laptop.</p>
+                      <p className='text-sm leading-tight text-muted-foreground'>Welcome to our laptop emporium, where we have revolutionized the way you shop for your perfect laptop</p>
                     </Link>
                   </NavigationMenuLink>
                 </li>
