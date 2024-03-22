5be5f34556e19cb12a5a0825382b193f33066ff9
rmmwalali <2003285@students.kcau.ac.ke>
2024-03-22 08:51:42 +0300

    Circuit

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -3,26 +3,13 @@ import { LucideProps, User } from 'lucide-react'
 export const Icons = {
   user: User,
   logo: (props: LucideProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circuit-board">
+  <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circuit-board">
       <rect width="18" height="18" x="3" y="3" rx="2" />
       <path d="M11 9h4a2 2 0 0 0 2-2V3" />
       <circle cx="9" cy="9" r="2" />
       <path d="M7 21v-4a2 2 0 0 1 2-2h4" />
-      <circle cx="15" cy="15" r="2" /></svg>
-    // <svg
-    //   xmlns='http://www.w3.org/2000/svg'
-    //   viewBox='0 0 24 24'
-    //   strokeWidth='1.5'
-    //   stroke='currentColor'
-    //   fill='none'
-    //   strokeLinecap='round'
-    //   strokeLinejoin='round'
-    //   {...props}>
-    //   <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-    //   <circle cx='7' cy='15' r='2' />
-    //   <circle cx='17' cy='15' r='2' />
-    //   <path d='M3 9a2 1 0 0 0 2 1h14a2 1 0 0 0 2 -1' />
-    // </svg>
+      <circle cx="15" cy="15" r="2" />
+    </svg>
   ),
   gitHub: (props: LucideProps) => (
     <svg viewBox='0 0 438.549 438.549' {...props}>
